578076b506cf4fa7c2b3f9a54dd60ad9d47c210f
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-09-20 18:40:45 +0100

    update

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,6 +1,7 @@
 import SettingsContainer from "@/components/Settings/SettingsContainer";
 import { getSettings } from "@/lib/PowerHouse";
 import { Settings } from "@/types";
+import { Loader2 } from "lucide-react";
 import React from "react";
 
 const SettingsPage = async () => {
@@ -11,9 +12,17 @@ const SettingsPage = async () => {
     return <p>Error loading settings data.</p>;
   }
   return (
-    <main className="pt-5 pb-20 px-[3%] min-h-[100vh]">
-      <SettingsContainer data={data} />
-    </main>
+    <>
+      {data ? (
+        <main className="pt-5 pb-20 px-[3%] min-h-[100vh]">
+          <SettingsContainer data={data} />
+        </main>
+      ) : (
+        <div className="flex items-center justify-center h-[100vh]">
+          <Loader2 size={50} className="animate-spin" color="#F8AF24" />
+        </div>
+      )}
+    </>
   );
 };
 
