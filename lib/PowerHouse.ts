4a26fecef1ea62e596915a0aa41160bd32555b39
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-09-22 18:20:57 +0100

    update

--- lib/PowerHouse.ts
+++ lib/PowerHouse.ts
@@ -59,7 +59,7 @@ export const getSettings = async () => {
 export const updateSettings = async (payload: UpdateSettingsPayload) => {
   try {
     const response = await fetch(
-      "http://localhost:3000/api/settings",
+      "https://store24-admin-sepia.vercel.app/api/settings",
       {
         method: "PUT",
         headers: {
